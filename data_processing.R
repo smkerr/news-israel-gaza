2bb3571682a0c4985fe7cffb6d93d2a895f4aca4
stvkerr <steven.miles.k@gmail.com>
2023-11-28 19:59:07 +0100

    Add RTF to TXT conversion to data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -48,7 +48,7 @@ raw_path     <- "/data/raw/"
 # Packages ----------------------------------------------------------------
 
 # Packages to load
-pckg_to_load <- c("data.table", "lubridate") #this version also loads packages from the country files as these seemed to fail loading.
+pckg_to_load <- c("data.table", "here", "lubridate", "stringr", "striprtf",) #this version also loads packages from the country files as these seemed to fail loading.
 
 # Load packages silently
 suppressPackageStartupMessages(
@@ -63,9 +63,26 @@ setwd(wd)
 source("wrangling_function.R")
 
 # ===========================================================================
-#   xx) DO WE NEED A SECTION TO CONVERT TFR FILES IN TXT HERE?
+#   1) Convert RTF to TXT files
 # ===========================================================================
 
+# define file names
+files_in <- list.files(path = here("data/rtf")) # RTF files (input)
+files_out <- str_replace(files_in, "RTF", "txt") # TXT files (output)
+
+# for each RTF file...
+for (i in seq_along(files_in)) {
+  
+  # skip if TXT file already exists
+  if (file.exists(here("data/raw", files_out[i]))) next
+  
+  # read in RTF file
+  temp_txt <- read_rtf(here("data/rtf", files_in[i]))
+  
+  # save as TXT file
+  writeLines(temp_txt, here("data/raw", str_replace(files_out[i], "RTF", "txt")))
+  
+}
 
 # ===========================================================================
 #   2) Retrieve latest newspaper level data from original sources
